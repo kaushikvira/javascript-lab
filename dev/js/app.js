56d7f29591fd16d1ede0e8073def1ede70955354
Vira, Kaushik <Kaushik_Vira@intuit.com>
2015-12-07 13:47:02 -0800

    Lab 3.2 Done.

--- dev/js/app.js
+++ dev/js/app.js
@@ -6,7 +6,10 @@ require([], function() {
             username: "Kaushik Vira"
         },
         initialize: function() {
-            console.log("in init get call after setting default" + this.get("username") + this.defaults.username);
+            console.log("1.  in init get call after setting default" + this.get("username") + this.defaults.username);
+        },
+        userName: function() {
+            return this.get("username");
         }
 
     });
@@ -16,19 +19,22 @@ require([], function() {
     console.log(JSON.stringify(track_1));
 
     track_1 = new UserModel({username: "my Lic"});
+    track_1.on("change:username", function(user) {
+        console.log("Changed username " + user.previous("username") + " to " + user.userName());
+    });
 
-    console.log(JSON.stringify(track_1));
+    console.log("2." + JSON.stringify(track_1));
 
     track_1.set({username: "hello"});
+    console.log("3." + JSON.stringify(track_1));
 
-    console.log(JSON.stringify(track_1));
-
-    console.log(track_1.toJSON());
+    console.log("4." + track_1.toJSON());
 
     track_1.unset("username");
 
-    console.log(track_1.toJSON());
+    console.log("5." + track_1.toJSON());
 
     track_1.set({username: "hello"}, {silent: true});
+    console.log("6." + track_1.toJSON());
 
 });
