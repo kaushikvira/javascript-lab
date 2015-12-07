d19eb8521b2a2624824c69c787e7e6861a7e121a
Vira, Kaushik <Kaushik_Vira@intuit.com>
2015-12-07 15:26:51 -0800

    added HandleBar Template with example

--- dev/js/views/UserView.js
+++ dev/js/views/UserView.js
@@ -1,14 +1,13 @@
 define(["views/UserView"], function() {
-
     var UserView = Backbone.View.extend({
         el: ".target",
-        template: Handlebars.compile("Hello this is handlebar template"),
+        template: Handlebars.compile($('#entry-template').html()),
         events: {
             "click ": "select",
         },
         render: function() {
             console.log("render got called");
-            this.$el.html(this.template());
+            this.$el.html(this.template({title: "hello", body: "body"}));
         },
         select: function() {
             console.log("got it here.");
